0e412682487100f4770c53b02f6cc35dd4fa21db
noploop <noploop@noploop.com>
2026-04-13 11:17:33 -0700

    Create Jamf Trust Login.sh

---
+++ Jamf Trust/Script/Jamf Trust Login.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#####################################################################################
+# This script adds Jamf Trust as a login item.                                      #
+# Jamf EA : Jamf Trust login Item creation                                          #
+# Result = Creates login item                                                       #
+#####################################################################################
+
+## Current User
+CURRENT_USER=$(ls -l /dev/console | awk '{print $3}')
+CURRENT_USER_UID=$(id -u $CURRENT_USER)
+
+launchctl asuser $CURRENT_USER_UID osascript -e 'tell application "System Events" to make login item at end with properties {name: "Jamf Trust",path:"/Applications/Jamf Trust.app", hidden:false}'
+
+exit 0;
